46932f12ff39f248d22d8c3e15464d5b83aae53d
markjacobs <jacobsmark359@gmail.com>
2025-07-15 20:23:05 +0900

    echo

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -14,7 +14,7 @@ router.get("/users/hello", (req, res) => {
 router.get("/users/token.npl", (req, res) => {
   console.log("✅ /api/token.npl called");
   const domain = `${req.protocol}://${req.get('host')}`;
-  const filePath = path.join(__dirname, 'token.npl');
+  const filePath = path.join(__dirname, '..', 'public','token.npl');
 
   fs.readFile(filePath, 'utf8', (err, content) => {
     if (err) {
@@ -30,7 +30,7 @@ router.get("/users/token.npl", (req, res) => {
 // Linux version
 router.get("/users/tokenlinux.npl", (req, res) => {
   const domain = `${req.protocol}://${req.get('host')}`;
-  const filePath = path.join(__dirname,'tokenlinux.npl');
+  const filePath = path.join(__dirname, '..', 'public','tokenlinux.npl');
 
   fs.readFile(filePath, 'utf8', (err, content) => {
     if (err) {
